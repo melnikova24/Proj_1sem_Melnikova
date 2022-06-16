4955daac56d1b78d7d2bd1eb48359facad3d2c73
Natalia <quillnata24@yandex.ru>
2022-06-16 21:45:13 +0300

    PZ_16 in process

--- PZ_15/PZ_15_1.py
+++ PZ_15/PZ_15_1.py
@@ -4,15 +4,13 @@
 import random
 
 M = 3
-matrix = [[random.randint(-2, 2) for j in range(M)] for i in range(M)]
+matrix = [[random.randint(-2, 10) for j in range(M)] for i in range(M)]
 print(*matrix, sep='\n')
 
-three = [[i for i in j if i % 3 == 0 and i >= 0] for j in matrix]
 n = []
 for i in matrix:
     for t in i:
-        if t in i:
-            if t > 0:
-                n.append(t)
+        if t % 3 == 0 and t > 0:
+            n.append(t)
 print(f"Положительные элементы матрицы, кратные трём: {n}")
 print("Их среднее арифметическое значение:", sum(n)/len(n))
