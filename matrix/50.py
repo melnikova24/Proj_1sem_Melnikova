8b75c52717d26a48f4901ec0820c7d676d4e951a
Natalia <quillnata24@yandex.ru>
2022-06-18 13:23:16 +0300

    small commit

---
+++ matrix/50.py
@@ -0,0 +1,19 @@
+# В матрице элементы второго столбца возвести в квадрат
+
+import random
+
+stl = int(input("Введите кол-во столбцов: "))
+str = int(input("Введите кол-во строк: "))
+matrix = [[random.randint(-10, 10) for i in range(stl)] for j in range(str)]
+print('Исходная: ')
+for v in matrix:
+    print(v)
+
+for i in range(stl):
+    for j in range(str):
+        if i == 1:
+            matrix[j][i] = matrix[j][i] * matrix[j][i]
+
+print('Результат: ')
+for v in matrix:
+    print(v)
