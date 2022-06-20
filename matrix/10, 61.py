03cff941d7273aa86e4a1d56785f21169e2550b9
Natalia <quillnata24@yandex.ru>
2022-06-20 19:14:28 +0300

    small commit

--- matrix/10, 61.py
+++ matrix/10, 61.py
@@ -2,10 +2,9 @@
 
 import random
 
-stl = int(input('Введите количество столбцов: '))
-str = int(input('Введите количество строк: '))
-
-matrix = [[random.randint(-3, 10) for i in range(stl)] for j in range(str)]
+stl = int(input("Введите кол-во столбцов: "))
+str = int(input("Введите кол-во строк: "))
+matrix = [[random.randint(-10, 10) for i in range(stl)] for j in range(str)]
 print('Исходная: ')
 for v in matrix:
     print(v)
@@ -14,6 +13,7 @@ for g in range(stl):
     for t in range(str):
         if matrix[g][t] % 2 != 0:
             matrix[g][t] = 0
+
 print('Результат: ')
 for v in matrix:
     print(v)
