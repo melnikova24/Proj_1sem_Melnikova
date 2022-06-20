3e8a3d41439fa922206680b458266cba0940dbcb
Natalia <quillnata24@yandex.ru>
2022-06-20 18:40:20 +0300

    small commit

--- matrix/1, 38.py
+++ matrix/1, 38.py
@@ -16,9 +16,11 @@ for v in Matr2:
 for i in range(stl):
     n = []
     for j in range(str):
-        if i != 0 and i != str - 1 and j != 0 and j != stl - 1:
+        if i != 0 and i != stl - 1 and j != 0 and j != str - 1:
             n.append(Matr2[i][j])
     if n:
         Matr1.append(n)
+print('Элементы Matr1: ', Matr1)
 print('Matr1: ')
-print(Matr1)
+for v in Matr1:
+    print(v)
