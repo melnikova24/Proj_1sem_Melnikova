2e7cff59dba335cf0440bebd6b87a0c85f8763f3
Natalia <quillnata24@yandex.ru>
2022-06-20 19:22:03 +0300

    small commit

--- matrix/15, 56.py
+++ matrix/15, 56.py
@@ -4,16 +4,17 @@ import random
 
 stl = int(input("Введите кол-во столбцов: "))
 str = int(input("Введите кол-во строк: "))
-N = int(input("Введите номер столбца N: "))
+N = int(input("Введите номер столбца: "))
 N -= 1
-matrix = [[random.randint(1, 10) for x in range(stl)]for y in range(str)]
 
-print("Исходная матрица: ")
+matrix = [[random.randint(-10, 10) for i in range(stl)] for j in range(str)]
+print('Исходная: ')
 for v in matrix:
     print(v)
 
 for g in range(stl):
-    matrix[g][N] = matrix[g][N] * matrix[g][N]
-print("Результат: ")
+    matrix[g][N] = matrix[g][N] * 2
+
+print('Результат: ')
 for v in matrix:
     print(v)
