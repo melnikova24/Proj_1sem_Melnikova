ef69a101c8b167f01ce16f14fcd29e61648e3479
Natalia <quillnata24@yandex.ru>
2022-06-20 19:36:25 +0300

    small commit

--- matrix/16, 55.py
+++ matrix/16, 55.py
@@ -1,19 +1,19 @@
-# В матрице элементы второго столбца возвести в квадрат
+# В матрице элементы последней строки заменить на 0
 
 import random
 
 stl = int(input('Введите количество столбцов: '))
 str = int(input('Введите количество строк: '))
 
-matrix = [[random.randint(1, 10) for i in range(stl)] for j in range(str)]
+matrix = [[random.randint(-3, 10) for i in range(stl)] for j in range(str)]
 print('Исходная: ')
 for v in matrix:
     print(v)
 
 for i in range(stl):
     for j in range(str):
-        if i == 1:
-            matrix[j][i] = matrix[j][i] ** 2
+        if j == str - 1:
+            matrix[j][i] = 0
 
 print('Результат: ')
 for v in matrix:
