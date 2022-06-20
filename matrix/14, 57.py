13848d2bb9cf849b53bd062ca4d683b1431a0928
Natalia <quillnata24@yandex.ru>
2022-06-20 17:50:39 +0300

    small commit

--- matrix/14, 57.py
+++ matrix/14, 57.py
@@ -1,6 +1,6 @@
-# Сгенерировать матрицу, в которой нечетные элементы заменяются на 0
+# В матрице элементы последнего столбца заменить на -1.
 
-import  random
+import random         # проблемы с выбором последнего столбца
 
 stl = int(input('Введите количество столбцов: '))
 str = int(input('Введите количество строк: '))
@@ -10,10 +10,10 @@ print('Исходная: ')
 for v in matrix:
     print(v)
 
-for g in range(stl):
-    for t in range(str):
-        if matrix[g][t] % 2 != 0:
-            matrix[g][t] = 0
+for j in range(str):
+    for i in range(stl):
+        if i == stl - 1:
+            matrix[j][i] = -1
 
 print('Результат: ')
 for v in matrix:
