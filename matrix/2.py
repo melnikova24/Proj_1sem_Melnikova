1487b542d5b72c71389fbc1c75f51946df5f0627
Natalia <quillnata24@yandex.ru>
2022-06-20 19:08:40 +0300

    small commit

--- matrix/2.py
+++ matrix/2.py
@@ -1,20 +1,19 @@
-# В матрице отрицательные элементы возвести в квадрат.
+# В матрице отрицательные элементы возвести в квадрат
 
 import random
 
-stl = int(input('Введите колчетсво столбцов: '))
-str = int(input('Введите колчетсво строк: '))
+stl = int(input("Введите кол-во столбцов: "))
+str = int(input("Введите кол-во строк: "))
 
-matrix = [[random.randint(-4, 10) for i in range(stl)] for j in range(str)]
-print('Исходная матрица: ')
+matrix = [[random.randint(-10, 10) for i in range(stl)] for j in range(str)]
+print('Исходная: ')
 for v in matrix:
     print(v)
 
-for g in range(stl):      # проверяем элементы строк и столбиков на < 0
+for g in range(stl):
     for t in range(str):
         if matrix[g][t] < 0:
-            matrix[g][t] = matrix[g][t] * matrix[g][t]
-
+            matrix[g][t] = matrix[g][t] ** 2
 print('Результат: ')
 for v in matrix:
     print(v)
